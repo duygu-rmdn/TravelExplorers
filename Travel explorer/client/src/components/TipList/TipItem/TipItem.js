9c62ace420543a09cd9a64cae79a18258924160b
Duygu <duygumetin99@gmail.com>
2023-04-08 21:11:08 +0300

    Edit tip list item

--- Travel explorer/client/src/components/TipList/TipItem/TipItem.js
+++ Travel explorer/client/src/components/TipList/TipItem/TipItem.js
@@ -20,11 +20,6 @@ export const TipItem = ({
                     <div className="d-flex justify-content-between mb-3">
                         <h5 className="mb-0">{title}</h5>
                     </div>
-                    <div className="d-flex mb-3">
-                        <small className="border-end me-3 pe-3">
-                            <i className="fa fa-star text-primary" /> 3 Reviews
-                        </small>
-                    </div>
                     <p className="text-body mb-3">
                         {conclusion}
                     </p>
